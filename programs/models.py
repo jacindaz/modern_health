49e5fa91c3feaf017ceacf2a9905e0c2e0534bb0
Jacinda Zhong <jacindaz@gmail.com>
2020-05-20 14:55:27 -0700

    Models for Program, Section, Activity, Options + helper fields to re-use for dates

--- programs/models.py
+++ programs/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+import programs.helpers.date_fields as date_fields
 
-# Create your models here.
+class Option(date_fields.DefaultDateFields):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = "options"
+
+
+class Activity(date_fields.DefaultDateFields):
+    activity_type = models.CharField(max_length=100)
+    html_snippet = models.TextField(null=True)
+    question = models.TextField(null=True)
+    options = models.ManyToManyField(Option, related_name="activities_options")
+
+    class Meta:
+        db_table = "activities"
+
+
+class Section(date_fields.DefaultDateFields):
+    description = models.TextField()
+    image_url = models.TextField(null=False)
+    order_index = models.IntegerField(default=0)
+    activities = models.ManyToManyField(Activity, related_name="sections_activities")
+
+    class Meta:
+        db_table = "sections"
+
+
+class Program(date_fields.DefaultDateFields):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    sections = models.ManyToManyField(Section, related_name="program_sections")
+
+    class Meta:
+        db_table = "programs"
