1d3db42b331a7c6d442fca4a79b865651d338c46
Jacinda Zhong <jacindaz@gmail.com>
2020-05-21 12:11:49 -0700

    Django rest framework setup

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'programs.apps.ProgramsConfig',
     'django_extensions',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
