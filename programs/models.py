34a9b4797171eecc20f436b6dec1e4743afe3990
Jacinda Zhong <jacindaz@gmail.com>
2020-05-24 19:36:18 -0700

    Fix model programs_sections join table name, add ActivityType enum

--- programs/models.py
+++ programs/models.py
@@ -1,5 +1,8 @@
+from enum import Enum
+
 from django.db import models
 import programs.helpers.date_fields as date_fields
+
 
 class Option(date_fields.DefaultDateFields):
     name = models.CharField(max_length=200)
@@ -8,8 +11,13 @@ class Option(date_fields.DefaultDateFields):
         db_table = "options"
 
 
+class ActivityType(Enum):
+    HTML = "html"
+    MULT_CHOICE = "multiple_choice"
+
+
 class Activity(date_fields.DefaultDateFields):
-    activity_type = models.CharField(max_length=100)
+    activity_type = models.CharField(max_length=100, choices=[(activity, activity.value) for activity in ActivityType])
     html_snippet = models.TextField(null=True)
     question = models.TextField(null=True)
     options = models.ManyToManyField(Option, related_name="activities_options")
@@ -20,7 +28,7 @@ class Activity(date_fields.DefaultDateFields):
 
 class Section(date_fields.DefaultDateFields):
     description = models.TextField()
-    image_url = models.TextField(null=False)
+    image_url = models.TextField(null=False, blank=False)
     order_index = models.IntegerField(default=0)
     activities = models.ManyToManyField(Activity, related_name="sections_activities")
 
@@ -31,7 +39,7 @@ class Section(date_fields.DefaultDateFields):
 class Program(date_fields.DefaultDateFields):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    sections = models.ManyToManyField(Section, related_name="program_sections")
+    sections = models.ManyToManyField(Section, related_name="programs_sections")
 
     class Meta:
         db_table = "programs"
