01eed880585a1283ec3d364f9ba296ea79f45d64
Jacinda Zhong <jacindaz@gmail.com>
2020-05-25 17:10:50 -0700

    Add django-rest-framework for APIs

--- programs/urls.py
+++ programs/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 
 from . import views
+from django.urls import include, path
+from rest_framework import routers
+from programs import views
+
+router = routers.DefaultRouter()
+router.register(r"programs", views.ProgramViewSet)
+router.register(r"sections", views.SectionViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- programs/views.py
+++ programs/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from programs.models import Activity,Option,Program,Section
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the programs index.")
+from rest_framework import viewsets
+from rest_framework import permissions
+from programs.serializers import ProgramSerializer, SectionSerializer
+
+
+class ProgramViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Program.objects.all().prefetch_related('sections')
+    serializer_class = ProgramSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = []
+
+
+class SectionViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Section.objects.all()
+    serializer_class = SectionSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = []
