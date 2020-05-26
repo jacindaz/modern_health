4f1d8e8fcf0c45fdf8fda7b030986231c05e40d1
Jacinda Zhong <jacindaz@gmail.com>
2020-05-25 21:03:47 -0700

    Use drf-nested-routers for nested API resources

--- programs/urls.py
+++ programs/urls.py
@@ -2,14 +2,19 @@ from django.urls import path
 
 from . import views
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework_nested import routers
 from programs import views
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r"programs", views.ProgramViewSet)
-router.register(r"sections", views.SectionViewSet)
+
+
+programs_router = routers.NestedSimpleRouter(router, r'programs', lookup='program')
+programs_router.register(r'sections', views.SectionViewSet, basename='program-sections')
+
 
 urlpatterns = [
+    path('', include(programs_router.urls)),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- programs/views.py
+++ programs/views.py
@@ -9,20 +9,20 @@ from programs.serializers import ProgramSerializer, SectionSerializer
 
 
 class ProgramViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = Program.objects.all().prefetch_related('sections')
     serializer_class = ProgramSerializer
-    # permission_classes = [permissions.IsAuthenticated]
     permission_classes = []
 
 
 class SectionViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
-    # permission_classes = [permissions.IsAuthenticated]
     permission_classes = []
+
+    def get_queryset(self):
+        program_id = self.kwargs['program_pk']
+        sections_for_program = Section.objects.all() \
+                               .prefetch_related('programs')\
+                               .filter(programs=program_id)
+
+        return sections_for_program
